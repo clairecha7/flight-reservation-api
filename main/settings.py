db86387fd7d11aa552df74b9cc46cc992447a6c0
clairecha7 <mybarclaire@gmail.com>
2023-05-22 18:21:01 -0700

    add postgres setting

--- main/settings.py
+++ main/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": config('SQL_DATABASE'),
+        "USER": config('SQL_USER'),
+        "PASSWORD": config('SQL_PASSWORD'),
+        "HOST": config('SQL_HOST'),
+        "PORT": config('SQL_PORT'),
     }
 }
 
